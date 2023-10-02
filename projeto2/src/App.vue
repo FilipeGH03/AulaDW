bff3c84b64a2427a261e95aac205698165772271
Filipe Hollmann <116088308+FilipeGH03@users.noreply.github.com>
2023-10-02 08:37:11 -0300

    Start

---
+++ projeto2/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import life from './components/life.vue';
+import newcom from './components/newcom.vue';
+export default{
+  name: "app",
+  components:{
+    life,
+    newcom
+    }
+}
+  
+</script>
+
+<template>
+<life></life>
+<newcom></newcom>
+</template>
+
+<style scoped>
+
+</style>
---
+++ projeto2/src/components/newcom.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p> {{ pessoa }}</p>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+    export default{
+        name: "newcom",
+        data(){
+            return{
+                pessoa: "voue"
+            }
+        }
+    }
+</script>
