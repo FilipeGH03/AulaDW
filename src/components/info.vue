69ead618e514b2574c5560550e68673aaaaf5dc9
WilliamKunzler <wkwilliamkunzler@gmail.com>
2023-10-09 08:25:16 -0300

    dale

--- src/components/info.vue
+++ src/components/info.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <p>Ela utiliza as seguintes tecnologias:</p>
+        <p v-if="trabalhar">Não quero trabalhar</p>
         <ul>
             <li>JavaScript</li>
             <li>Vue</li>
@@ -14,7 +15,7 @@
         name: "Info",
         data() {
             return {
-
+                trabalhar: true,
             }
         }
     }
