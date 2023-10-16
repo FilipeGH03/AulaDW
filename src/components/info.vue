1e618483a245fe454966fc647f8709376b523d9f
WilliamKunzler <wkwilliamkunzler@gmail.com>
2023-10-16 07:57:29 -0300

    ola

--- src/components/info.vue
+++ src/components/info.vue
@@ -2,11 +2,14 @@
     <div>
         <p>Ela utiliza as seguintes tecnologias:</p>
         <p v-if="trabalhar">Não quero trabalhar</p>
+        <p v-else >Não quero faze nada</p>
         <ul>
             <li>JavaScript</li>
             <li>Vue</li>
             <li>HTML</li>
         </ul>
+        <p v-show="mostrarEmail">Fala cos guri ai{{email}}</p>
+        <p>Para acessar meu curriculo click aqui, <a v-bind:href="link">Aqui songo mongo</a> </p>
     </div>
 </template>
 
@@ -15,7 +18,10 @@
         name: "Info",
         data() {
             return {
-                trabalhar: true,
+                trabalhar: false,
+                mostrarEmail: true,
+                email: "iouiysufdyghiajoskpd@gmail.com",
+                link: 'https://www.chess.com'
             }
         }
     }
