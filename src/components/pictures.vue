706254bc469804807448ef824d503322539d2dab
WilliamKunzler <wkwilliamkunzler@gmail.com>
2023-11-20 10:33:38 -0300

    timeline

--- src/components/pictures.vue
+++ src/components/pictures.vue
@@ -1,9 +1,11 @@
 <template>
     <img :src="avatar" alt="descricao">
+    <MudarImagem @mudar-imagem="trocarImagem"></MudarImagem>
 </template>
 
 
 <script>
+import MudarImagem from './MudarImagem.vue';
     export default {
         name: 'pictures',
         data(){
@@ -11,6 +13,13 @@
                 avatar:'img/avatar.png',
                 descricao: "avatar de cria"
             }
-        }
-    }
+        },
+        components:{
+            MudarImagem
+        },
+
+        methods:{
+            trocarImagem(){
+            this.avatar = "img/avatar2.png"
+        }, }}
 </script>
